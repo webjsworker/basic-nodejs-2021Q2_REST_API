6c9ad76279c0f47cabf871a3c3e1e60be8688569
webjsworker <47504749+webjsworker@users.noreply.github.com>
2021-05-23 16:30:41 +0300

    Update user.service.js

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,9 +1,10 @@
 const usersRepo = require('./user.memory.repository');
+const User = require('./user.model')
 
 const getAll = () => usersRepo.getAll();
 const getById = id => usersRepo.getById(id);
-const create = user => usersRepo.create(user);
-const deleteById = id => usersRepo.deleteById(id);
-const ChangeUser = (id, body) =>  usersRepo.ChangeUser(id, body);
+const creat = user => usersRepo.creat(new User(user))
+const deleteById = id => usersRepo.deleteById(id)
+const change = (id, user) => usersRepo.change(id, user)
 
-module.exports = { getAll , getById, create, deleteById, ChangeUser };
+module.exports = { getAll, getById, deleteById, creat, change};
