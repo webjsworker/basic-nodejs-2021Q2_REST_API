88f13d49db175717afb10af6df8fb19f9831bae4
webjsworker <47504749+webjsworker@users.noreply.github.com>
2021-05-23 16:35:33 +0300

    Update board.model.js

--- src/resources/board/board.model.js
+++ src/resources/board/board.model.js
@@ -1,24 +1,28 @@
-const uuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+const Сolumn = require('./column.model')
+
+function createColumn(option) {
+  const column = []
+  for(let i=0; i < option.length; i +=1) {
+    column.push(new Сolumn(option[i]))
+  }
+  return column
+}
 
 class Board {
   constructor({
-    id = uuid.v4(),
-    title = 'string',
-    columns = [{}]  
-      } = {}) 
-  {
+    id = uuidv4(),
+    title = 'BOARD',
+    columns = []
+  } = {}) {
     this.id = id;
     this.title = title;
-    this.columns = columns;
-          
+    this.columns = createColumn(columns)
   }
-    /* static addTask(){
-        class Task {}
-    } */
-  /* static toResponse(user) {
-    const { id, name, login } = user;
-    return { id, name, login };
-  } */
-}
 
+  static toResponse(board) {
+    const {id, title, columns} = board
+    return {id, title, columns}
+  }
+}
 module.exports = Board;
