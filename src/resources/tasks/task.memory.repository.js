37b23340fdee604f5c9e1696422c0633760b55fd
webjsworker <47504749+webjsworker@users.noreply.github.com>
2021-05-23 16:34:09 +0300

    Update task.memory.repository.js

--- src/resources/tasks/task.memory.repository.js
+++ src/resources/tasks/task.memory.repository.js
@@ -1,25 +1,44 @@
-const DBTasks = require('./inMemoryTasks');
+// const Task = require('./tasks.model')
 
-const getAlltask = async () =>  DBTasks;
+let DBTasks = []
+
+const getAll = (boardId) => DBTasks.filter(all => all.boardId === boardId);
+
+const getById = async (boardId, id) => {
+  const task = await DBTasks.find(ts => ts.boardId === boardId && ts.id === id)
+  return task
+}
 
 const create = async (task) => {
-  DBTasks.push(task)
-  return task;
-};
-  
-const getById = async (id) => {
-  const task = await  DBTasks.filter(el => el.id === id )[0];
-return task
-};
-  
-/* const changetask = async (taskid, body) => {
-  const task = await  DBTasks.filter(el => el.id === taskid )[0];
-  const changetask = Object.assign(task, body);
-  
-  return changetask
-} */
- 
+  DBTasks = [...DBTasks, task]
+  return task
+}
 
+const deleteById = async (boardId, id) => {
+  DBTasks = await DBTasks.filter(ts => ts.id !== id && ts.boardId === boardId  )
+}
 
+const deleteTasksBoard = async (boardId) => {
+  DBTasks = await DBTasks.filter(ts => ts.boardId !== boardId)
+}
 
-module.exports = { getAlltask, create, getById,     };
+const deleteUserTasks =  (userId) => {
+  DBTasks =  DBTasks.map(obj => {
+    if (obj.userId === userId) {
+      return {...obj, userId: null};
+    }
+    return obj
+  })
+}
+
+const change = async (boardId, id, task) => {
+  DBTasks = DBTasks.map(obj => {
+    if (obj.id === id && obj.boardId === boardId) {
+      return {id, ...task};
+    }
+    return obj
+  })
+  return getById(boardId, id)
+}
+
+module.exports = { getAll, getById, create, deleteById, change, deleteTasksBoard, deleteUserTasks };
