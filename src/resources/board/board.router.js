f315e78fbf80f92d28d417573d298f828e8b3349
webjsworker <47504749+webjsworker@users.noreply.github.com>
2021-05-23 16:35:51 +0300

    Update board.router.js

--- src/resources/board/board.router.js
+++ src/resources/board/board.router.js
@@ -1,38 +1,33 @@
 const router = require('express').Router();
-const Board = require('./board.model')
-const boardService = require('./board.service');
+const Board = require('./boards.model');
+const boardsService = require('./boards.service');
 
 router.route('/').get(async (req, res) => {
-  const boards = await boardService.getAllBoard();
-  res.json(boards);
+  const boards = await boardsService.getAll();
+  await res.json(boards.map(Board.toResponse));
+});
+
+router.route('/:id').get(async (req, res) => {
+  const board = await boardsService.getById(req.params.id);
+  if(!board) {
+    res.status(404).json()
+  }
+  res.status(200).send(board);
+});
+
+router.route('/:id').delete(async (req, res) => {
+  await boardsService.deleteById(req.params.id);
+  res.sendStatus(200)
 });
 
 router.route('/').post(async (req, res) => {
-    const board = await boardService.creat(
-      new Board ({
-        title: req.body.title,
-        columns: req.body.columns,
-        
-      })
-    );
-    res.status(201).json(board);
-  });
+  const board = await boardsService.creat(req.body);
+  res.status(201).send(board);
+});
 
-
-  router.route('/:id').get(async (req, res) => {
-    const board = await boardService.getById(req.params.id);
-    res.json(board);
-  });
-
-  router.route('/:id').put(async (req, res) => {
-     const board = await boardService.changeBoard(req.params.id, req.body);
-      res.json(board);
-     });
-
-     /* router.route('/:id').delete(async (req, res) => {
-        const board  = await boardService.deleteById(req.params.id);
-         
-          res.json(board);
-        }); */
+router.route('/:id').put(async (req, res) => {
+  const board = await boardsService.change(req.params.id, req.body );
+  res.status(200).send(board);
+});
 
 module.exports = router;
