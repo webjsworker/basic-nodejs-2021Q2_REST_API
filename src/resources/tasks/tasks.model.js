b6627c679566f01343f4f006f5910ddbe1616832
Aliaksei <aliakseiyudziankou@gmail.com>
2021-05-28 17:44:19 +0300

    jsdoc ./tasks

--- src/resources/tasks/tasks.model.js
+++ src/resources/tasks/tasks.model.js
@@ -1,6 +1,20 @@
 const { v4: uuidv4 } = require('uuid');
 
+/**
+ * This is class of task
+ * @class
+ */
 class Task {
+  /**
+   * 
+   * @param {string} id - task's id 
+   * @param {string} title - task's title  
+   * @param {namber} order - task's order  
+   * @param {string} descroption - task's description
+   * @param {sring} userId - user's id 
+   * @param {string} boardId - board's id 
+   * @param {string} columnId - column id 
+   */
   constructor({
     id = uuidv4(),
     title = 'TASK',
@@ -19,6 +33,11 @@ class Task {
     this.columnId = columnId;
   }
 
+/**
+ * This function retern task's elemets
+ * @param {object} task - tasks elemets 
+ * @returns {object} - object of task 
+ */
   static toResponse(task) {
     const {id, title, order, description, userId, boardId, columnId} = task
     return {id, title, order, description, userId, boardId, columnId}
